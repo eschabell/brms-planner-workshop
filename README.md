b22c6890393b06fe906c0ca1b2476afbae2730f2
Eric D. Schabell <eric@schabell.org>
2015-02-19 13:35:27 +0100

    Initial import workshop.

---
+++ README.md
@@ -0,0 +1,29 @@
+Business Resource Planner Workshop
+==================================
+A workshop to get you started with the JBoss BRMS Business Resource Planner component.
+
+
+Install on your machine
+-----------------------
+1. [Download and unzip.](https://github.com/eschabell/brms-planner-workshop/archive/master.zip)
+
+2. Add products to installs directory.
+
+3. Run 'init.sh' or 'init.bat' file. 'init.bat' must be run with Administrative privileges.
+
+4. Read and follow instructions shown during installation.
+
+
+Supporting Articles
+-------------------
+None yet.
+
+
+Released versions
+-----------------
+See the tagged releases for the following versions of the product:
+
+- v1.0 - Business Resource Planner workshop based on JBoss BRMS 6.0.3 release.
+
+![Cover](https://github.com/eschabell/brms-planner-workshop/blob/master/docs/demo-images/cover.png?raw=true)
+
---
+++ init.sh
@@ -0,0 +1,69 @@
+#!/bin/sh 
+DEMO="JBoss BRMS Business Resource Planner Workshop"
+AUTHORS="Geoffrey de Smet, Eric D. Schabell"
+PROJECT="git@github.com:eschabell/brms-planner-workshop.git"
+PRODUCT="JBoss BRMS Business Resource Planner"
+JBOSS_HOME=./target
+SUPPORT_DIR=./support
+SRC_DIR=./installs
+PRJ_DIR=./projects
+SOURCE=jboss-bpms-brms-6.0.3.GA-redhat-1-optaplanner.zip
+VERSION=6.0.3
+
+# wipe screen.
+clear 
+
+echo
+echo "##########################################################"
+echo "##                                                      ##"   
+echo "##  Setting up the                                      ##"
+echo "##                                                      ##"   
+echo "##   ${DEMO}      ##"
+echo "##                                                      ##"   
+echo "##                                                      ##"   
+echo "##   ####   #       ###   #   #  #   #  #####  ####     ##"
+echo "##   #   #  #      #   #  ##  #  ##  #  #      #   #    ##"
+echo "##   ####   #      #####  # # #  # # #  ####   ####     ##"
+echo "##   #      #      #   #  #  ##  #  ##  #      #  #     ##"
+echo "##   #      #####  #   #  #   #  #   #  #####  #   #    ##"
+echo "##                                                      ##"   
+echo "##                                                      ##"   
+echo "##  brought to you by,                                  ##"   
+echo "##        ${AUTHORS}            ##"
+echo "##                                                      ##"   
+echo "##  ${PROJECT}  ##"
+echo "##                                                      ##"   
+echo "##########################################################"
+echo
+
+command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
+
+# make some checks first before proceeding.	
+if [ -r $SRC_DIR/$SOURCE ] || [ -L $SRC_DIR/$SOURCE ]; then
+	echo JBoss product sources, $BRMS present...
+	echo
+else
+	echo Need to download $SOURCE package from the Customer Portal 
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+
+# Remove old instance if found.
+if [ -x $JBOSS_HOME ]; then
+	echo "  - existing install detected and removed..."
+	echo
+	rm -rf ./target
+fi
+
+# Install product.
+echo "Installing workshop..."
+echo
+unzip -q -d $JBOSS_HOME $SRC_DIR/$SOURCE
+
+echo "$PRODUCT can be found in $JBOSS_HOME..."
+echo 
+echo "$PRODUCT $VERSION $DEMO Setup Complete."
+echo
+
